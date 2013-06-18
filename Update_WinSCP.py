1d430bb8c2f693251f042f0bc998d399aa23cc11
John Shaw <j2897@mailinator.com>
2013-06-18 05:20:55 +0100

    Bug fixed and minor enhancements.
    Fixed: Wasn't downloading if no WinSCP was not already installed.
    Added: Visual output indicating the progress.
    Added: 5 second delay before ending the program.
    Added: 'download_install()' procedure.

--- Update_WinSCP.py
+++ Update_WinSCP.py
@@ -9,10 +9,16 @@ title = 'WinSCP Updater'
 target = 'Downloading WinSCP'
 url = 'http://winscp.net/eng/download.php'
 
+print 'Running:		' + title
+print 'Target:			' + target
+print 'URL:			' + url
+
 try:
 	page = get_page(url)
 except:
 	page = None
+else:
+	print 'Got page...'
 
 def msg_box(message, box_type):
 	import win32api
@@ -40,7 +46,10 @@ def find_site_ver(page):
 try:
 	site_version, FN = find_site_ver(page)
 except:
-	site_version = None
+	msg_box('Could not search the page.', 0)
+	stop()
+else:
+	print 'Found:			' + site_version
 
 if site_version == None:
 	msg_box('The search target has not been found on the page. The formatting, or the text on the page, may have been changed.', 0)
@@ -49,28 +58,88 @@ if site_version == None:
 import os
 tmp = os.getenv('TEMP')
 PF = os.getenv('PROGRAMFILES')
+WinSCP_exe = PF + '\\WinSCP\\WinSCP.exe'
+DL = tmp + '\\' + FN
+command = [DL, '/SILENT', '/NORESTART']
 
-def clean(text):
-	import re
-	return re.sub('[^0-9]', '', text)
+def DL_file():
+	import urllib
+	url = 'http://downloads.sourceforge.net/project/winscp/WinSCP/' + site_version + '/' + FN
+	urllib.urlretrieve(url, DL)
+
+def sub_proc(command):
+	import subprocess
+	p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+	stdout, stderr = p.communicate()
+	return p.returncode # is 0 if success
+
+def download_install():
+	try:
+		DL_file()
+	except:
+		msg_box('Failed to download ' + FN + ' to ' + tmp + '.', 0)
+		stop()
+	else:
+		print 'Downloaded:		' + FN
+
+	try:
+		RC = sub_proc(command)
+	except:
+		RC = None
+
+	if RC == None:
+		msg_box('Failed to execute ' + FN + '.', 0)
+		stop()
+	elif RC == 0:
+		msg_box('Successfully updated to version ' + site_version + '.', 0)
+		stop()
+	else:
+		msg_box('Successfully spawned new process for ' + FN + '. But the installation appears to have failed.', 0)
+		stop()
+
+# Check if the WinSCP.exe file exists...
+if not os.path.isfile(WinSCP_exe):
+	# No:	Download and install WinSCP, and quit.
+	print 'WinSCP.exe file doesn\'t exist.'
+	print 'Installing WinSCP for the first time...'
+	download_install()
+	print 'Ending...'
+	delay(5)
+	stop()
 
 import win32api
 try:
-    info = win32api.GetFileVersionInfo(PF + '\\WinSCP\\WinSCP.exe', "\\")
+    info = win32api.GetFileVersionInfo(WinSCP_exe, "\\")
     ms = info['FileVersionMS']
     ls = info['FileVersionLS']
     file_version = "%d.%d.%d.%d" % (win32api.HIWORD(ms), win32api.LOWORD (ms),
 									win32api.HIWORD (ls), win32api.LOWORD (ls))
 except:
-    file_version = None
-
-if file_version == None:
 	msg_box('Cannot find the file version of the local WinSCP.exe file.', 0)
 	stop()
+else:
+	print 'Got local file version information...'
 
 # Check if the site_version numbers are in the file_version numbers...
-if clean(file_version).find(clean(site_version)) != -1:
+def clean(text):
+	import re
+	return re.sub('[^0-9]', '', text)
+
+clean_site_version = clean(site_version)
+clean_file_version = clean(file_version)[:len(clean_site_version)]
+
+print 'Local version:		' + clean_file_version
+print 'Site version:		' + clean_site_version
+
+def delay(sec):
+	import time
+	time.sleep(sec)
+
+if clean_file_version.find(clean_site_version) != -1:
 	# Yes:	Quit.
+	print 'Match!'
+	print 'Ending...'
+	delay(5)
 	stop()
 
 # Check if WinSCP is running...
@@ -83,6 +152,7 @@ def find_proc(exe):
 			return True
 
 while find_proc('WinSCP.exe'):
+	print 'WinSCP is running. Close WinSCP now!'
 	user_input = msg_box('There is a new version of WinSCP available. Please close WinSCP and press OK to continue.', 1)
 	if user_input == 1:
 		pass
@@ -90,39 +160,10 @@ while find_proc('WinSCP.exe'):
 		stop()
 
 # Now download and install the new file...
-user_input = msg_box('If you have a WinSCP.ini file, back it up now and then press OK when you are ready to proceed with the update.', 1)
+user_input = msg_box('If you use a custom WinSCP.ini file, back it up now and then press OK when you are ready to proceed with the update.', 1)
 if user_input == 2:
 	stop()
 
-import urllib
-url = 'http://downloads.sourceforge.net/project/winscp/WinSCP/' + site_version + '/' + FN
-DL = tmp + '\\' + FN
-
-try:
-	urllib.urlretrieve(url, DL)
-except:
-	msg_box('Failed to download [' + FN + '] to [' + tmp + '].', 0)
-	stop()
-
-def sub_proc(exe, args):
-	import subprocess
-	filepath = exe + args
-	p = subprocess.Popen(filepath, shell=True, stdout=subprocess.PIPE)
-	stdout, stderr = p.communicate()
-	return p.returncode # is 0 if success
-
-args = ' /SILENT /NORESTART'
-
-try:
-	RC = sub_proc(DL, args)
-except:
-	RC = None
-
-if RC == None:
-	msg_box('Failed to execute [' + FN + '].', 0)
-	stop()
-elif RC == 0:
-	msg_box('Successfully updated from version [' + file_version + '] to version [' + site_version + '].', 0)
-	stop()
-else:
-	msg_box('Successfully executed [' + FN + ']. But the installation appears to have failed.', 0)
+download_install()
+print 'Ending...'
+delay(5)
